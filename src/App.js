78f462e94c6b970e763ec0f7074d8813473a0e71
sujinnyan <ansujinny@gmail.com>
2022-07-27 12:07:29 -0400

    edited file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import "./App.css"
 import Footer from "./components/Footer"
 import Signup from "./components/Signup"
 import Signin from "./components/Signin"
-import Home from "./components/Hero/Home";
+import "../src/components/Hero/Home.css"
+import { Typewriter } from 'react-simple-typewriter'
 
 
 
@@ -15,7 +16,50 @@ function App() {
   return (
     <>
     <Header />
-    <Home/>
+    <section className='hero' id='home'>  
+          <div className='container f_flex top'>
+              <div className='left top'>
+                  <h3>WELCOME TO FORM.</h3>
+                  <h1>Hello, We are <span>FORM. </span></h1>
+                  <h2>
+                      a
+                      <span>
+                        
+                        <Typewriter
+                        words={[' Professional ', ' Unique', ' Essencial' ]}
+                        loop={3}
+                        cursor
+                        cursorStyle='|'
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                        />
+                    
+                    </span>
+
+                </h2>
+
+                 <p>Today, I will be walking you through the creation of a login page using HTML, CSS and JavaScript. But don’t worry. This is your first login page, which means the code is as simple as possible and there will be an explanation to accompany each piece of code.</p>
+
+          <div className='hero_btn'>
+              <div className='col_1'>
+                  
+                  <div className='button'>
+                      <button className='btn_shadow'>
+                          <i className='fab fa-facebook-f'><a href="#"></a></i>
+                      </button>
+                      <button className='btn_shadow'>
+                          <i className='fab fa-instagram'><a href="#"></a></i>
+                      </button>
+                      <button className='btn_shadow'>
+                          <i className='fab fa-youtube'><a href="#"></a></i>
+                      </button>
+                  </div>
+              </div>
+          </div>
+        </div>
+      </div>
+      </section>
     <ToastContainer />
     <Routes>
     <Route path="/Signup" element={<Signup />}/>
